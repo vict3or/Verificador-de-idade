5a436284d4f931aadef0c7de6bd4b5553e4ab06e
Victor Oliveira <oliveira.vitoco@gmail.com>
2021-03-02 13:06:13 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,50 @@
+var data = document.getElementById('nascimento')
+var fsex = document.getElementsByName('radsex')
+var btn = document.getElementById('btn_verificar')
+var res = document.getElementById('res')
+var img = document.createElement('img')
+img.setAttribute('id', 'foto')
+
+
+
+btn.addEventListener('click', verificador)
+
+ 
+function verificador(){
+     var date = new Date();
+     var ano = date.getFullYear()
+     idade = ano - parseInt(data.value) 
+    if(parseInt(data.value) > 2021){
+        alert('[ERRO] Verifique os dados e tente novamente!')
+    }else{
+          var genero = ''
+          if(fsex[0].checked){
+               genero = 'Homem'
+               if(idade >= 0 && idade < 10){
+                    img.setAttribute('src', 'imagens/crianca-homem.PNG')
+               }else if(idade >= 10 && idade < 21){
+                    img.setAttribute('src', 'imagens/jovem-homem.PNG')
+               }else if(idade < 50){
+                    img.setAttribute('src', 'imagens/adulto-homem.PNG')
+               }else{
+                    img.setAttribute('src', 'imagens/idoso-homem.PNG')
+               }
+          }else if(fsex[1].checked){
+               genero = 'Mulher'
+               if(idade >= 0 && idade < 10){
+                    img.setAttribute('src', 'imagens/crianca-mulher.PNG')
+               }else if(idade >= 10 && idade < 21){
+                    img.setAttribute('src', 'imagens/jovem-mulher.PNG')
+               }else if(idade < 50){
+                    img.setAttribute('src', 'imagens/adulto-mulher.PNG')
+               }else{
+                    img.setAttribute('src', 'imagens/idoso-mulher.PNG')
+               }
+          }
+                    
+          res.style.textAlign = 'center'
+          res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+          res.appendChild(img)
+    }
+    
+}
