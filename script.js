abf15bda44941329469332a92342ac4857f84500
Victor Oliveira <oliveira.vitoco@gmail.com>
2021-03-02 13:15:51 -0300

    segundo commit

--- script.js
+++ script.js
@@ -21,24 +21,24 @@ function verificador(){
           if(fsex[0].checked){
                genero = 'Homem'
                if(idade >= 0 && idade < 10){
-                    img.setAttribute('src', 'imagens/crianca-homem.PNG')
+                    img.setAttribute('src', 'imagens/crianca-homem.png')
                }else if(idade >= 10 && idade < 21){
-                    img.setAttribute('src', 'imagens/jovem-homem.PNG')
+                    img.setAttribute('src', 'imagens/jovem-homem.png')
                }else if(idade < 50){
-                    img.setAttribute('src', 'imagens/adulto-homem.PNG')
+                    img.setAttribute('src', 'imagens/adulto-homem.png')
                }else{
-                    img.setAttribute('src', 'imagens/idoso-homem.PNG')
+                    img.setAttribute('src', 'imagens/idoso-homem.png')
                }
           }else if(fsex[1].checked){
                genero = 'Mulher'
                if(idade >= 0 && idade < 10){
-                    img.setAttribute('src', 'imagens/crianca-mulher.PNG')
+                    img.setAttribute('src', 'imagens/crianca-mulher.png')
                }else if(idade >= 10 && idade < 21){
-                    img.setAttribute('src', 'imagens/jovem-mulher.PNG')
+                    img.setAttribute('src', 'imagens/jovem-mulher.png')
                }else if(idade < 50){
-                    img.setAttribute('src', 'imagens/adulto-mulher.PNG')
+                    img.setAttribute('src', 'imagens/adulto-mulher.png')
                }else{
-                    img.setAttribute('src', 'imagens/idoso-mulher.PNG')
+                    img.setAttribute('src', 'imagens/idoso-mulher.png')
                }
           }
                     
